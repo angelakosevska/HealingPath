0805b180989d6699f2ac8635b0a6121207884f62
Angela <angelakjosevska@gmail.com>
2025-05-12 21:04:21 +0200

    main activity fragments styled

--- app/src/main/java/com/example/healingpath/activities/MainActivity.java
+++ app/src/main/java/com/example/healingpath/activities/MainActivity.java
@@ -1,30 +1,65 @@
 package com.example.healingpath.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import com.example.healingpath.R;
-import com.example.healingpath.activities.LoginActivity;
-import com.google.firebase.auth.FirebaseAuth;
+import com.example.healingpath.fragments.CalendarFragment;
+import com.example.healingpath.fragments.InjuriesFragment;
+import com.example.healingpath.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonLogout;
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonLogout = findViewById(R.id.buttonLogout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        buttonLogout.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
-        });
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        // Load default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new InjuriesFragment())
+                    .commit();
+        }
     }
+
+    private final NavigationBarView.OnItemSelectedListener navListener =
+            item -> {
+                Fragment selectedFragment;
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_calendar) {
+                    selectedFragment = new CalendarFragment();
+                } else if (itemId == R.id.nav_injuries) {
+                    selectedFragment = new InjuriesFragment();
+                } else if (itemId == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                } else {
+                    return false;
+                }
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+
+                return true;
+            };
+
 }
--- app/src/main/java/com/example/healingpath/fragments/InjuriesFragment.java
+++ app/src/main/java/com/example/healingpath/fragments/InjuriesFragment.java
@@ -1,4 +1,27 @@
 package com.example.healingpath.fragments;
 
-public class InjuriesFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.healingpath.R;
+
+public class InjuriesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_injuries, container, false);
+
+        // TODO: initialize RecyclerView or Button to add new injury
+
+        return root;
+    }
 }
